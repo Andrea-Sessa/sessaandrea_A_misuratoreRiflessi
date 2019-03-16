8afb53e88d72ee16f0ce127d6281e7be2814368f
Sessa-Andrea <andrea.sessa@barsanti.edu.it>
2019-03-16 10:47:33 +0100

    update
    creazione funzioni

--- sessaandrea_A_misuratoreRiflessi/sessaandrea_A_misuratoreRiflessi.ino
+++ sessaandrea_A_misuratoreRiflessi/sessaandrea_A_misuratoreRiflessi.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+int led = 13;
+int button = 4;
+int buzzer = 8;
+int tempo = 1000;
+int a = 0;
+
+void setup() {
+  // put your setup code here, to run once
+  pinMode(led, OUTPUT);
+  pinMode(button, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+void lampeggio()
+{
+  delay(tempo);
+  digitalWrite(led, HIGH);
+  while (digitalRead(button) == LOW);
+  digitalWrite(led, LOW);
+  while (digitalRead(button) == HIGH);
+}
+void suono()
+{
+ delay(tempo);
+ tone(buzzer, 1000, 5000);
+ while (digitalRead(button) == LOW);
+ noTone(buzzer);
+ while (digitalRead(button) == HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  lampeggio();
+  suono();
 
 }
