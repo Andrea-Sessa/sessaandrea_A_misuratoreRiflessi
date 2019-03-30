5e96b87154463c5a16557761a57ac091381a383c
Sessa-Andrea <andrea.sessa@barsanti.edu.it>
2019-03-30 10:49:50 +0100

    ultimo update

--- sessaandrea_A_misuratoreRiflessi/sessaandrea_A_misuratoreRiflessi.ino
+++ sessaandrea_A_misuratoreRiflessi/sessaandrea_A_misuratoreRiflessi.ino
@@ -6,7 +6,6 @@ int buzzer = 8;
 int tempo = 1000;
 int tempo1 = 0;
 int tempo2 = 0;
-int stato = 0;
 const int a = 2, b = 6, c = 3, d = 5, e = 9, f = 10;
 LiquidCrystal lcd(a, b, c, d, e, f);
 
@@ -15,52 +14,53 @@ void setup() {
   pinMode(led, OUTPUT);
   pinMode(button, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  lcd.begin(16, 2);
-  
+  lcd.begin(16,2);
 }
-
+void lampeggio()
+{
+  bool vero = true;
+  delay(tempo);
+  digitalWrite(led, HIGH);
+  tempo1 = millis();
+  while (digitalRead(button) == LOW);
+  digitalWrite(led, LOW);
+  while (digitalRead(button) == HIGH);
+  {
+    if (vero)
+    {
+      tempo2 = millis() - tempo1;
+      vero = false;
+    }
+  }
+  lcd.setCursor(0,0);
+  lcd.print(tempo2);
+}
+void suono()
+{
+ bool vero = true;
+ delay(tempo);
+ tone(buzzer, 1000, 5000);
+ tempo1 = millis();
+ while (digitalRead(button) == LOW);
+ noTone(buzzer);
+ while (digitalRead(button) == HIGH);
+ {
+  if (vero)
+    {
+      tempo2 = millis() - tempo1;
+      vero = false;
+    }
+ }
+  lcd.setCursor(0,1);
+  lcd.print(tempo2);}
+ 
+}
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+  lampeggio();
+  suono();
   
-  delay(tempo);
-  digitalWrite(led, HIGH);
-  tempo1 = millis();
-  while(stato == 0)
-  {
-    
-    if (digitalRead(button) == HIGH)
-    {
-      
-      stato = 1;
-    }
-    
-  }
-  if (stato == 1)
-  {
-    digitalWrite(led, LOW);
-  }
-
-  lcd.setCursor(0,0);
-  tempo2 = millis() - tempo1;
-  lcd.print(tempo2);
-
-  delay(tempo);
-  tone(buzzer, 1000, 5000);
-  tempo1 = millis();
-  while (stato == 0)
-  {
-    if(digitalRead(button) == HIGH)
-    {
-      noTone(buzzer);
-    }
-  
-  }
-  
-
-  lcd.setCursor(0,0);
-  tempo2 = millis() - tempo1;
- lcd.print(tempo2);
 
 }
